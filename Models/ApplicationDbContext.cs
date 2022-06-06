5a46efb2dddc3f7cd3459482f0913c4d94eb4c71
A <33346753+Alqatf@users.noreply.github.com>
2022-06-06 11:42:59 -0700

    Add files via upload

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AH_Project.Models.Entities;
+
+namespace AH_Project.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        public DbSet<ModelCaption> ModelCaptions { get; set; } = null!;
+        public DbSet<SimilarCaption> SimilarCaptions { get; set; } = null!;
+        public DbSet<Evaluation> Evaluations { get; set; } = null!;
+
+    }
+}
---
+++ Models/Services/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AH_Project.Models.Entities;
+
+namespace AH_Project.Models.Services.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<ModelCaption> ModelCaptions { get; }
+        IRepository<SimilarCaption> SimilarCaptions { get; }
+        IRepository<Evaluation> Evaluations { get; }
+        int Complete();
+    }
+}
